8e7ab1b841c94efbb95ad44267f17b8fa235e576
Chun <heliochun@users.noreply.github.com>
2019-07-15 02:37:27 +0100

    Muffin 2.2 (Moonlight)

--- muffin/js/kronos.js
+++ muffin/js/kronos.js
@@ -1,9 +1,22 @@
 
 document.addEventListener('DOMContentLoaded', function(e) {
 	var learnM = document.getElementById('learn-muffin');
-	var ytMuffin = '<div class="yt-rapper"><iframe width="560" height="349" src="https://www.youtube.com/embed/szKzz60srDQ?rel=0&hd=1&autoplay=1" frameborder="0" allowfullscreen></iframe></div>';
+	var videoImage1 = document.getElementById('video1');
+	var videoImage2 = document.getElementById('video2');
+	
+	var ytMuffin = '<div class="yt-wrapper"><iframe width="560" height="349" src="https://www.youtube.com/embed/9pP0vyPbzww?rel=0&hd=1&autoplay=1" frameborder="0" allowfullscreen></iframe></div>';
+	var ytMuffin1 = '<div class="yt-wrapper"><iframe width="560" height="349" src="https://www.youtube.com/embed/szKzz60srDQ?rel=0&hd=1&autoplay=1" frameborder="0" allowfullscreen></iframe></div>';
+	var ytMuffin2 = '<div class="yt-wrapper"><iframe width="560" height="349" src="https://www.youtube.com/embed/9pP0vyPbzww?rel=0&hd=1&autoplay=1" frameborder="0" allowfullscreen></iframe></div>';
 
 	learnM.addEventListener('click', function(){
 		learnM.innerHTML = ytMuffin;
+	});
+	
+	videoImage1.addEventListener('click', function(){
+		learnM.innerHTML = ytMuffin1;
+	});
+	
+	videoImage2.addEventListener('click', function(){
+		learnM.innerHTML = ytMuffin2;
 	});
 });
