31e7c54a15ae0ff1ec37bd4a6c2950d1b1b1e10b
Hélio Chun <heliochun@users.noreply.github.com>
2019-10-08 23:55:24 +0100

    08-10-2019

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+
 document.addEventListener('DOMContentLoaded', function(e) {
 	
 	var getSocial = document.getElementById('getSocial');
@@ -16,3 +17,17 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 });
 
+// Too lazy at this hour of the night for Vanilla
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+});
