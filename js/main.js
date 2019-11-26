5656c7324629768e1392e39e34f251daf0c1e2b8
Hélio Chun <heliochun@users.noreply.github.com>
2019-11-26 18:54:05 +0000

    .

--- js/main.js
+++ js/main.js
@@ -10,6 +10,16 @@ $(document).ready(function(){
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
-	})
+	});
+	
+	$('.mobileSelectedNav').click(function(){
+		$('.mobileNav').toggleClass('showMenu');
+	});
+	
+	$('body,html').click(function(event) {
+		if ($(event.target).closest('.mobileSelectedNav, .mobileNav').length === 0) {
+			$('.mobileNav').removeClass('showMenu');
+		}
+	});
 
 });
