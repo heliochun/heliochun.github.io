d350561b9bdae6de1ae30d06f6b45a13c1f71c77
Hélio Chun <heliochun@users.noreply.github.com>
2020-03-24 06:24:07 +0000

    .

--- js/social.js
+++ js/social.js
@@ -1,18 +1,17 @@
 
 document.addEventListener('DOMContentLoaded', function(e) {
 	
-	var getSocial = document.getElementById('getSocial');
 	var showSocial = document.getElementById('showSocial');
 	var body = document.getElementById('body');
 
-	getSocial.addEventListener('click', function(){
-		showSocial.classList.toggle("show");
+	$('.getSocial').on('click', function(){
+		$('#showSocial').toggleClass('show');
 	});
 	
 	// Close Social Panel
 	
 	showSocial.addEventListener('click', function(){
-		showSocial.classList.toggle("show");
+		showSocial.classList.toggle('show');
 	});
 
 });
